f0b7241de7440c1518e81af65488abf473cdbea2
SharminAkter3 <mssharmin4145@gmail.com>
2024-01-25 19:00:52 +0600

    updated

--- covid_plus/urls.py
+++ covid_plus/urls.py
@@ -18,11 +18,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import HomeView
+from .views import HomeView, about, contact
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", HomeView.as_view(), name="home"),
+    path("about/", about, name="about"),
+    path("contact/", contact, name="contact"),
     path("account/", include("account.urls")),
     path("campaign/", include("campaign.urls")),
 ]
--- covid_plus/views.py
+++ covid_plus/views.py
@@ -8,33 +8,6 @@ from django.views.generic import View
 # Create your views here.
 
 
-# class CampaignListView(ListView):
-#     model = Campaign
-#     template_name = "home.html"
-#     context_object_name = "campaigns"
-
-
-# class CampaignReviewView(LoginRequiredMixin, FormView):
-#     template_name = "home.html"
-#     form_class = CampaignReviewForm
-#     success_url = reverse_lazy("home")
-
-#     def form_valid(self, form):
-#         reviewer = self.request.user
-#         new_comment = form.save(commit=False)
-#         new_comment.reviwer = reviewer
-#         new_comment.save()
-#         return super().form_valid(form)
-
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         reviews = Review.objects.all()  # Fetch all reviews
-#         comment_form = kwargs.get("form", CampaignReviewForm())
-#         context["reviews"] = reviews
-#         context["comment_form"] = comment_form
-#         return context
-
-
 class HomeView(View):
     template_name = "home.html"
 
@@ -50,3 +23,11 @@ class HomeView(View):
         }
 
         return render(request, self.template_name, context)
+
+
+def about(request):
+    return render(request, "aboutUs.html")
+
+
+def contact(request):
+    return render(request, "contact.html")
